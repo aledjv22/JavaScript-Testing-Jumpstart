5df090503ac5a07859e439ebe2efffcbd361d8ae
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-02-29 11:42:36 -0300

    Add mathematical functions with corresponding tests

---
+++ demos/src/02-math.test.js
@@ -0,0 +1,80 @@
+const {sum, multiply, divide, my_module} = require('./02-math');
+
+test("adds 1 + 3 should equal 4", () => {
+  const rta = sum(1, 3);
+  expect(rta).toBe(4);
+});
+
+test("adds -1 + 3 should equal 2", () => {
+  const rta = sum(-1, 3);
+  expect(rta).toBe(2);
+});
+
+test("multiply 2 * 3 should equal 6", () => {
+  const rta = multiply(2, 3);
+  expect(rta).toBe(6);
+});
+
+test("multiply -2 * 3 should equal -6", () => {
+  const rta = multiply(-2, 3);
+  expect(rta).toBe(-6);
+});
+
+test("divide 6 / 3 should equal 2", () => {
+  const rta = divide(6, 3);
+  expect(rta).toBe(2);
+});
+
+test("divide -6 / 3 should equal -2", () => {
+  const rta = divide(-6, 3);
+  expect(rta).toBe(-2);
+});
+
+test("divide 6 / -3 should equal -2", () => {
+  const rta = divide(6, -3);
+  expect(rta).toBe(-2);
+});
+
+test("divide -6 / -3 should equal 2", () => {
+  const rta = divide(-6, -3);
+  expect(rta).toBe(2);
+});
+
+test("divide any number by 0 should return null", () => {
+  let rta = divide(6, 0);
+  expect(rta).toBeNull();
+
+  rta = divide(-6, 0);
+  expect(rta).toBeNull();
+});
+
+test("module 6 % 3 should equal 0", () => {
+  const rta = my_module(6, 3);
+  expect(rta).toBe(0);
+});
+
+test("module 6 % 4 should equal 2", () => {
+  const rta = my_module(6, 4);
+  expect(rta).toBe(2);
+});
+
+test("module -6 % 10 should equal 6", () => {
+  const rta = my_module(-6, 10);
+  expect(rta).toBe(-6);
+});
+
+test("module 6 % -10 should equal 6", () => {
+  const rta = my_module(6, -10);
+  expect(rta).toBe(6);
+});
+
+test("module -6 % -10 should equal -6", () => {
+  const rta = my_module(-6, -10);
+  expect(rta).toBe(-6);
+});
+
+test("module 6 % 0 should return null", () => {
+  const rta = my_module(6, 0);
+  expect(rta).toBeNull();
+});
+
