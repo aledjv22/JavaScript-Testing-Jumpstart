d15a6004f591f9e80790b1622a0b11f33975907c
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-02-29 21:45:54 -0300

    Integrated describe in the math tests

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -2,80 +2,90 @@ const {
   sum, multiply, divide, myModule,
 } = require('./02-math');
 
-test('adds 1 + 3 should equal 4', () => {
-  const rta = sum(1, 3);
-  expect(rta).toBe(4);
-});
+describe('Test for Math', () => {
+  describe('Tests for sum', () => {
+    test('adds 1 + 3 should equal 4', () => {
+      const rta = sum(1, 3);
+      expect(rta).toBe(4);
+    });
 
-test('adds -1 + 3 should equal 2', () => {
-  const rta = sum(-1, 3);
-  expect(rta).toBe(2);
-});
+    test('adds -1 + 3 should equal 2', () => {
+      const rta = sum(-1, 3);
+      expect(rta).toBe(2);
+    });
+  });
 
-test('multiply 2 * 3 should equal 6', () => {
-  const rta = multiply(2, 3);
-  expect(rta).toBe(6);
-});
+  describe('Tests for multiply', () => {
+    test('multiply 2 * 3 should equal 6', () => {
+      const rta = multiply(2, 3);
+      expect(rta).toBe(6);
+    });
 
-test('multiply -2 * 3 should equal -6', () => {
-  const rta = multiply(-2, 3);
-  expect(rta).toBe(-6);
-});
+    test('multiply -2 * 3 should equal -6', () => {
+      const rta = multiply(-2, 3);
+      expect(rta).toBe(-6);
+    });
+  });
 
-test('divide 6 / 3 should equal 2', () => {
-  const rta = divide(6, 3);
-  expect(rta).toBe(2);
-});
+  describe('Tests for divide', () => {
+    test('divide 6 / 3 should equal 2', () => {
+      const rta = divide(6, 3);
+      expect(rta).toBe(2);
+    });
 
-test('divide -6 / 3 should equal -2', () => {
-  const rta = divide(-6, 3);
-  expect(rta).toBe(-2);
-});
+    test('divide -6 / 3 should equal -2', () => {
+      const rta = divide(-6, 3);
+      expect(rta).toBe(-2);
+    });
 
-test('divide 6 / -3 should equal -2', () => {
-  const rta = divide(6, -3);
-  expect(rta).toBe(-2);
-});
+    test('divide 6 / -3 should equal -2', () => {
+      const rta = divide(6, -3);
+      expect(rta).toBe(-2);
+    });
 
-test('divide -6 / -3 should equal 2', () => {
-  const rta = divide(-6, -3);
-  expect(rta).toBe(2);
-});
+    test('divide -6 / -3 should equal 2', () => {
+      const rta = divide(-6, -3);
+      expect(rta).toBe(2);
+    });
 
-test('divide any number by 0 should return null', () => {
-  let rta = divide(6, 0);
-  expect(rta).toBeNull();
+    test('divide any number by 0 should return null', () => {
+      let rta = divide(6, 0);
+      expect(rta).toBeNull();
 
-  rta = divide(-6, 0);
-  expect(rta).toBeNull();
-});
+      rta = divide(-6, 0);
+      expect(rta).toBeNull();
+    });
+  });
 
-test('module 6 % 3 should equal 0', () => {
-  const rta = myModule(6, 3);
-  expect(rta).toBe(0);
-});
+  describe('Tests for myModule', () => {
+    test('module 6 % 3 should equal 0', () => {
+      const rta = myModule(6, 3);
+      expect(rta).toBe(0);
+    });
 
-test('module 6 % 4 should equal 2', () => {
-  const rta = myModule(6, 4);
-  expect(rta).toBe(2);
-});
+    test('module 6 % 4 should equal 2', () => {
+      const rta = myModule(6, 4);
+      expect(rta).toBe(2);
+    });
 
-test('module -6 % 10 should equal 6', () => {
-  const rta = myModule(-6, 10);
-  expect(rta).toBe(-6);
-});
+    test('module -6 % 10 should equal 6', () => {
+      const rta = myModule(-6, 10);
+      expect(rta).toBe(-6);
+    });
 
-test('module 6 % -10 should equal 6', () => {
-  const rta = myModule(6, -10);
-  expect(rta).toBe(6);
-});
+    test('module 6 % -10 should equal 6', () => {
+      const rta = myModule(6, -10);
+      expect(rta).toBe(6);
+    });
 
-test('module -6 % -10 should equal -6', () => {
-  const rta = myModule(-6, -10);
-  expect(rta).toBe(-6);
-});
+    test('module -6 % -10 should equal -6', () => {
+      const rta = myModule(-6, -10);
+      expect(rta).toBe(-6);
+    });
 
-test('module 6 % 0 should return null', () => {
-  const rta = myModule(6, 0);
-  expect(rta).toBeNull();
+    test('module 6 % 0 should return null', () => {
+      const rta = myModule(6, 0);
+      expect(rta).toBeNull();
+    });
+  });
 });
