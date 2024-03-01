2e4d11fb9c7a5ccc20bd01c0fac25bc6300e580e
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-01 10:28:35 -0300

    AAA

--- demos/src/06-person.test.js
+++ demos/src/06-person.test.js
@@ -3,13 +3,17 @@ const Person = require('./06-person');
 describe('Test for Person', () => {
   let person;
 
+  // Arrange
   beforeEach(() => {
     person = new Person('Alejandro', 45, 1.7);
   });
 
   test('should return down', () => {
+    // Arrange
     person.weight = 45;
+    // Act
     const imc = person.calcIMC();
+    // Assert
     expect(imc).toBe('down');
   });
 
