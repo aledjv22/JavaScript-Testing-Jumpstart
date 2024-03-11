0a475d6873f32da7160e4152f5308c6923334a6b
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-11 09:37:52 -0300

    Add spies to test books functionality

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -7,12 +7,17 @@ const fakeBooks = [
   },
 ];
 
-const MongoLibStub = {
-  getAll: () => [...fakeBooks],
-  create: () => {},
-};
+const mockGetAll = jest.fn();
 
-jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+// const MongoLibStub = {
+//   getAll: mockGetAll,
+//   create: () => {},
+// };
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: mockGetAll,
+  create: () => {},
+})));
 
 describe('Test for BooksService', () => {
   let service;
@@ -25,12 +30,15 @@ describe('Test for BooksService', () => {
   describe('Test for getBooks', () => {
     test('should return a list of books', async () => {
       // Arrage
+      mockGetAll.mockResolvedValue(fakeBooks);
       // Act
       const books = await service.getBooks({});
       console.log('books: ', books);
-
       // Assert
       expect(books.length).toEqual(1);
+      expect(mockGetAll).toHaveBeenCalled();
+      expect(mockGetAll).toHaveBeenCalledTimes(1);
+      expect(mockGetAll).toHaveBeenCalledWith('books', {});
     });
   });
 });
