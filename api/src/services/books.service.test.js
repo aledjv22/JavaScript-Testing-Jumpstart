b62d56c865fdda7cf0fe9c748c12964233cce868
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2024-03-11 02:49:44 -0300

    Implemented mocking

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,10 +1,25 @@
 const BooksService = require('./books.service');
 
+const fakeBooks = [
+  {
+    _id: 1,
+    name: 'Harry Potter',
+  },
+];
+
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => {},
+};
+
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+
 describe('Test for BooksService', () => {
   let service;
 
   beforeEach(() => {
     service = new BooksService();
+    jest.clearAllMocks();
   });
 
   describe('Test for getBooks', () => {
@@ -15,7 +30,7 @@ describe('Test for BooksService', () => {
       console.log('books: ', books);
 
       // Assert
-      expect(books.length).toEqual(13);
+      expect(books.length).toEqual(1);
     });
   });
 });
